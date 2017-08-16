82ef95bb8c903687883f27a743e8fe3bd67ba838
Xina-Torres <xina.torres@gmail.com>
2017-08-16 11:56:44 +0800

    if-case p 191

---
+++ SwiftChallenge4.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+var age = 21
+if case 18...29 = age, age >= 21, age <= 29 {
+    print("In cool demographic and of drinking age")
+}else{
+    print("Other")
+}
